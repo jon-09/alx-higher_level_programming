8c00a94f28be005833f9f6fedbcb150c83ff35e2
jonathan <achola.jon@yahoo.com>
2022-06-27 19:54:26 +0000

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#inlcude "list'h"
+/**
+*check_cycle - checks whether the linked list has a loop
+*@list: the list to be checked
+*Return: 0 if no loop is found , 1 otherwise
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *CurHead, *NxtHead;
+	CurHead = NxtHead = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (CurHead && NxtHead && NxtHead->next)
+	{
+		CurHead = CurHead->next;
+		NxtHead = NxtHead->next;
+		
+		if (CurHead == NxtHead)
+			return (1);
+
+	}
+	return (0);
+}
