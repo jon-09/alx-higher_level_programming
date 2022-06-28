4bf000ae76941e55022157f8535ca839e82ba64c
jonathan <achola.jon@yahoo.com>
2022-06-28 14:00:27 +0000

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(1, 101):
     if i % 15 == 0:
-        print("FizzBzz", end="")
+        print("FizzBuzz", end="")
     elif i % 3 == 0:
         print("Fizz", end="")
     elif i % 5 == 0:
@@ -9,5 +9,5 @@ for i in range(1, 101):
     else:
         print(i, end="")
     print(" ", end="")
-print("")
+print()
 
