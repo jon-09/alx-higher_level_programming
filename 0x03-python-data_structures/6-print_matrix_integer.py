1ca92efca9087fd675e8a1e7df00a8dade07226e
Jonathan <achola.jon@yahoo.com>
2022-07-05 12:26:17 +0300

    tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-
+    for row in matrix:
+        for col in row:
+            print(f"{col:d}", sep="", end="")
+            if col != row[len(row) - 1]:
+                print(" ", end="")
+        print()
