e260ccfa3b6a19cbdcfd01b4ef902d3dc7d78089
Jonathan <achola.jon@yahoo.com>
2022-07-05 13:41:41 +0300

    tassk

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = len(tuple_a)
-    b = len(tuple_b)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
 
-    tupl = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
-            (tuple)a[1] if a >= 1 else 0) + (tuple_b[1] if b >= 1 else 0))
-    return tupl
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+    
+    newT = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return newT
