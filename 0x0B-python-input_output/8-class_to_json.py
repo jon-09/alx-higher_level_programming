2c2a46841c58c97f2050e2aec69c9e8029d3723f
Jonathan <achola.jon@yahoo.com>
2022-08-02 17:18:41 +0300

    fixing exception erro

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" A module that contains a function "class_to_json(obj)" """
-import json
+""" Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
+"""
 
 
 def class_to_json(obj):
-    """ A function that returns the dictionary description with simple
-    data structures like (list, dictionary, strion, integer and boolean)
-    for JSON serialization of an object """
+    """ Function that retuns the dictionary description of an obj """
 
-    return obj.__dict__
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
