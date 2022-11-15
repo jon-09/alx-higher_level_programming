1c630803c9759761a378cdae6c60751fc4549318
jon-09 <achola.jon@yahoo.com>
2022-11-15 17:16:23 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-/* script that reads and prints the content of a file. */
+/*
+script that reads and prints the content of a file.
+*/
 const file = process.argv[2];
 const fs = require('fs');
 
