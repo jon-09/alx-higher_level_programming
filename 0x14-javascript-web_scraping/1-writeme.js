1ef026e71bd2948f5d3d55854137db50b5447f9c
jon-09 <achola.jon@yahoo.com>
2022-11-16 17:46:32 +0300

    write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ a script that writes a string to a file
+ */
+
+const file = process.argv[2];
+const string = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(file, string, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+});
