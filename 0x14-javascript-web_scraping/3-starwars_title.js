385fc0a8b5a9e68845b0d2db36fdf9ba246ce5a4
jon-09 <achola.jon@yahoo.com>
2022-11-16 17:51:35 +0300

    star wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ a script that prints the title of a Star Wars movie where the episode number matches a given integer
+ */
+
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+const axios = require('axios');
+
+axios.get(url).then(res => {
+  console.log(res.data.title);
+}).catch(err => {
+  console.log('code: ' + err.res.status);
+});
