9e3f1c40bf20f6b29091b354d37a6234e93fbbcf
jon-09 <achola.jon@yahoo.com>
2022-11-16 18:00:12 +0300

    wars wedge

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
-const url = process.argv[2];
-const axios = require('axios').default;
+/*
+a script that prints the number of movies where the character “Wedge Antilles” is present
+*/
 
-axios.get(url).then(function (res) {
-  const movies = res.data.results;
-  let counter = 0;
-  for (let i = 0; i < movies.length; i++) {
-    for (let x = 0; x < movies[i].characters.length; x++) {
-      if (movies[i].characters[x].includes('18')) {
-        counter++;
-        break;
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
       }
     }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  console.log(counter);
-}).catch(function (error) {
-  console.log(`code: ${error.response.status}`);
 });
